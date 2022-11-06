4c0c3211ac48803d9d2e9562300423a54f9e370a
Juan Felipe Jimenez P. <pipesjimenez6@gmail.com>
2022-11-06 18:21:24 -0500

    inicializando el proyecto

---
+++ src/main/java/com/trabajoGrado/TrabajoGradoApplication.java
@@ -0,0 +1,13 @@
+package com.trabajoGrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoGradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoGradoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trabajoGrado/TrabajoGradoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabajoGrado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoGradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
