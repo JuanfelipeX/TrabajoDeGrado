48b116e1298f7272ac898e12221d3de96811cb31
Juan Felipe Jimenez P. <pipesjimenez6@gmail.com>
2022-11-06 20:01:52 -0500

    haciendo los metodos del servicio usuarios

---
+++ src/main/java/com/trabajoGrado/service/UsuariosService.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.trabajoGrado.service;
+
+import com.trabajoGrado.model.Usuarios;
+import com.trabajoGrado.repository.UsuariosRepository;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author pipes
+ */
+
+@Service
+@RequiredArgsConstructor
+public class UsuariosService {
+    private final UsuariosRepository usuariosRepository;
+    
+    public void save(Usuarios usuarios){
+        usuariosRepository.save(usuarios);
+    }
+    
+    public List<Usuarios> findAll(){
+        return usuariosRepository.findAll();
+    } 
+    
+    public Optional<Usuarios> findById(Integer id){
+        return usuariosRepository.findById(id);
+    }
+    
+    public void deleteById(Integer id){
+        usuariosRepository.deleteById(id);
+    }
+    
+}
