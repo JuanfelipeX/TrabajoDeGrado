48e1f425f415c112db9b70f72fda08b2edfd304e
Juan Felipe Jimenez P. <pipesjimenez6@gmail.com>
2022-11-06 19:02:53 -0500

    haciendo los atributos de del modelo Usuarios

---
+++ src/main/java/com/trabajoGrado/model/Usuarios.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.trabajoGrado.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ *
+ * @author pipes
+ */
+@Document(value = "Usuarios")
+@Data
+public class Usuarios {
+    @Id
+    private Integer id;
+    private String nombres;
+    private String apellidos;
+    private String email;
+    private Integer edad;
+    
+}
