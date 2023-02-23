b8824f1d33c216565a157e0ebe31c9af2e216dd2
Juan Felipe Jimenez P. <pipesjimenez6@gmail.com>
2023-02-23 18:46:31 -0500

    arreglando el service de herramientas

---
+++ src/main/java/com/trabajoGrado/service/HerramientasService.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.trabajoGrado.service;
+
+import com.trabajoGrado.model.Herramientas;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import com.trabajoGrado.repository.HerramientasRepository;
+
+/**
+ *
+ * @author pipes
+ */
+@Service
+@RequiredArgsConstructor
+public class HerramientasService {
+
+    private final HerramientasRepository herramientasRepository;
+
+    public void save(Herramientas herramientas) {
+        herramientasRepository.save(herramientas);
+    }
+
+    public List<Herramientas> findAll() {
+        return herramientasRepository.findAll();
+    }
+
+    public Optional<Herramientas> findById(String id) {
+        return herramientasRepository.findById(id);
+    }
+
+    public Optional<Herramientas> updatedId(Herramientas req, String id) {
+        Optional<Herramientas> herramientas = herramientasRepository.findById(id);
+        if (herramientas != null) {
+            herramientasRepository.save(req);
+        }
+        return herramientas;
+    }
+
+    public void deleteById(String id) {
+        herramientasRepository.deleteById(id);
+    }
+}
